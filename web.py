747692b4dc4ab93f0693c93712d5015cb3981a6a
Jarek Černý <cerny.jarek@gmail.com>
2024-04-20 17:35:47 +0200

    Clean todos.txt

--- web.py
+++ web.py
@@ -12,8 +12,10 @@ todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("<h1>This app is to increase your <b>productivity</b>.</h1>")
 
+st.text_input(label="Enter a todo", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,6 +24,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label="Enter a todo", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
