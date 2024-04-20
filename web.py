7c9362b71523cc1a93e07e358a80e0104ef0f763
Jarek Černý <cerny.jarek@gmail.com>
2024-04-20 17:37:31 +0200

    Clean todos.txt

--- web.py
+++ web.py
@@ -12,7 +12,8 @@ todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("<h1>This app is to increase your <b>productivity</b>.</h1>")
+st.write("<h1>This app is to increase your <b>productivity</b>.</h1>",
+         unsafe_allow_html=True)
 
 st.text_input(label="Enter a todo", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
